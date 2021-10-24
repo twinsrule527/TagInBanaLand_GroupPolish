4c4cef6bc8785519ab57d8800b7d50225f2f2ad2
Eli <eliclendenning@gmail.com>
2021-10-24 17:06:20 -0400

    Controller input, at game start

---
+++ Assets/EliWork/Scripts/ShadowPos.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Used to get the shadow of an object's position displayed accurately
+public class ShadowPos : MonoBehaviour
+{
+    [SerializeField] private float y_offset;
+    [SerializeField] private Transform characterTransform;
+    void LateUpdate()
+    {
+        transform.position = new Vector3(transform.parent.position.x, transform.parent.position.y + y_offset, characterTransform.position.z +0.1f);
+    }
+}
